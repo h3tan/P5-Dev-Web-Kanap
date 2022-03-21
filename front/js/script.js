7bdde65015eccae88058708e48048dce6650e9b3
Thi Herve <thi.h3rve@gmail.com>
2022-03-21 17:23:11 +0100

    Ajout de fonctions pour la lisibilité de script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,55 @@
 /**
+ * Créé une balise <element>, si des attributs sont présents, implante ces attributs dans la balise
+ * @param {String} element
+ * @param {Array} attribut : Array of String
+ * @param {Array} attributName : Array of String
+ * Taille de attribut et attributName doivent être égales
+ * @returns {HTMLElement}
+ */
+function newElement(element, attribut, attributName) {
+  let newElt = document.createElement(element);
+  if (arguments.length < 2) {
+    return newElt;
+  } else {
+    for (i in attribut) {
+      newElt.setAttribute(attribut[i], attributName[i]);
+    }
+    return newElt;
+  }
+}
+
+/**
+ *  Ajoute <element> comme enfant de <parent> à la fin de ses autres enfants
+ * @param {HTMLElement} parent
+ * @param {HTMLElement} element
+ * @returns {NHTMLElement}
+ */
+function append(parent, element) {
+  return parent.appendChild(element);
+}
+
+/**
+ *  Crée un nouvel attribut contentName pour <element> ayant pour valeur contentText
+ * @param {HTMLElement} element
+ * @param {String} contentName
+ * @param {String} contentText
+ */
+function newAttribute(element, contentName, contentText) {
+  element.setAttribute(contentName, contentText);
+}
+
+/**
+ *  Ajoute text comme contenu de <element>
+ * @param {HTMLElement} element
+ * @param {String} text
+ */
+function setText(element, text) {
+  element.textContent = text;
+}
+
+let blocArticles = document.getElementById("items");
+
+/**
  * Effectue une requête sur l'API et récupère au format JSON les produits de l'API.
  * En cas d'erreur, affiche dans la page d'accueil l'impossibilité d'afficher les produits
  */
@@ -13,35 +64,40 @@ fetch("http://localhost:3000/api/products/")
    * Détails des produits affichés: id, image, nom et description.
    */
   .then(function (produits) {
-    let produit = document.getElementById("items");
+    console.log(produits);
     for (let i in produits) {
-      let lienCanape = document.createElement("a");
-      lienCanape.setAttribute("href", `./product.html?id=` + produits[i]._id);
+      let lienCanape = newElement(
+        "a",
+        ["href"],
+        [`./product.html?id=` + produits[i]._id]
+      );
 
-      let canape = document.createElement("article");
+      let canape = newElement("article");
 
-      let imageProduit = document.createElement("img");
-      imageProduit.setAttribute("src", produits[i].imageUrl);
-      imageProduit.setAttribute("alt", produits[i].altTxt);
+      let imageProduit = newElement(
+        "img",
+        ["src", "alt"],
+        [produits[i].imageUrl, produits[i].altTxt]
+      );
 
-      let nomProduit = document.createElement("h3");
-      nomProduit.setAttribute("class", "productName");
+      let nomProduit = newElement("h3", ["class"], ["productName"]);
 
-      let descriptionProduit = document.createElement("p");
-      descriptionProduit.setAttribute("class", "productDescription");
+      let descriptionProduit = newElement(
+        "p",
+        ["class"],
+        ["produitDescription"]
+      );
 
-      nomProduit.textContent = produits[i].name;
-      descriptionProduit.textContent = produits[i].description;
+      setText(nomProduit, produits[i].name);
+      setText(descriptionProduit, produits[i].description);
 
-      canape.appendChild(imageProduit);
-      canape.appendChild(nomProduit);
-      canape.appendChild(descriptionProduit);
-      lienCanape.appendChild(canape);
-
-      produit.appendChild(lienCanape);
+      append(blocArticles, lienCanape);
+      append(lienCanape, canape);
+      append(canape, imageProduit);
+      append(canape, nomProduit);
+      append(canape, descriptionProduit);
     }
   })
   .catch(function (err) {
-    let produit = document.getElementById("items");
-    produit.innerHTML = "Impossible d'afficher les produits";
+    blocArticles.innerHTML = "Impossible d'afficher les produits";
   });
