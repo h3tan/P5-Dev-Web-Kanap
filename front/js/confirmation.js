58d9423097f0372a70579d5b00b86a45d198970a
Thi Herve <thi.h3rve@gmail.com>
2022-04-11 16:22:13 +0200

    Ajout de commentaire dans confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,6 @@
 let url = new URL(document.location);
 
+// Récupération du numéro de commande dans l'url de la page et affichage sur la page
 document.getElementById("orderId").textContent =
   url.searchParams.get("orderId");
 localStorage.removeItem("cart");
